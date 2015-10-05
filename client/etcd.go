e9795337087d64598d9a6b0686da6ae582e3a5e3
Marin Atanasov Nikolov <dnaeon@gmail.com>
2015-10-05 14:10:30 +0300

    Initial commit of the etcd client

---
+++ client/etcd.go
@@ -0,0 +1,48 @@
+package client
+
+import (
+	"encoding/json"
+	"path/filepath"
+
+	// "github.com/dnaeon/gru/minion"
+	"gru/minion"
+
+	"code.google.com/p/go-uuid/uuid"
+	"github.com/coreos/etcd/Godeps/_workspace/src/golang.org/x/net/context"
+	etcdclient "github.com/coreos/etcd/client"
+)
+
+type EtcdClient struct {
+	// etcd keys api client
+	KAPI etcdclient.KeysAPI
+}
+
+func NewEtcdClient(kapi etcdclient.KeysAPI) *EtcdClient {
+	c := &EtcdClient{
+		KAPI: kapi,
+	}
+
+	return c
+}
+
+func (c *EtcdClient) SubmitTask(u uuid.UUID, t minion.MinionTask) error {
+	minionRootDir := filepath.Join(minion.EtcdMinionSpace, u.String())
+	queueDir := filepath.Join(minionRootDir, "queue")
+
+	_, err := c.KAPI.Get(context.Background(), minionRootDir, nil)
+	if err != nil {
+		return err
+	}
+
+	data, err := json.Marshal(t)
+	if err != nil {
+		return err
+	}
+
+	_, err = c.KAPI.CreateInOrder(context.Background(), queueDir, string(data), nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
