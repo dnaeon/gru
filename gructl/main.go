9dd27cd2aae0e52d1d0cc7dbfe94c36ad76f69ff
Marin Atanasov Nikolov <dnaeon@gmail.com>
2015-10-23 16:41:16 +0300

    gructl: Add global flags for etcd endpoints, username and password

--- gructl/main.go
+++ gructl/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"time"
 
 	"github.com/dnaeon/gru/version"
 	"github.com/dnaeon/gru/gructl/command"
@@ -14,11 +15,36 @@ func main() {
 	app.Name = "gructl"
 	app.Version = version.Version
 	app.Usage = "command line tool for managing minions"
+
 	app.Flags = []cli.Flag{
-		cli.StringFlag{Name: "endpoint", Value: "http://127.0.0.1:2379,http://localhost:4001", Usage: "etcd cluster endpoints"},
+		cli.StringFlag{
+			Name: "endpoint",
+			Value: "http://127.0.0.1:2379,http://localhost:4001",
+			Usage: "etcd cluster endpoints",
+			EnvVar: "GRUCTL_ENDPOINT",
+		},
+		cli.StringFlag{
+			Name: "username",
+			Value: "",
+			Usage: "username to use when authenticating",
+			EnvVar: "GRUCTL_USERNAME",
+		},
+		cli.StringFlag{
+			Name: "password",
+			Value: "",
+			Usage: "password to use when authenticating",
+			EnvVar: "GRUCTL_PASSWORD",
+		},
+		cli.DurationFlag{
+			Name: "timeout",
+			Value: time.Second,
+			Usage: "connection timeout per request",
+			EnvVar: "GRUCTL_TIMEOUT",
+		},
 	}
+
 	app.Commands = []cli.Command{
-		command.NewLsCommand(),
+		command.NewMinionCommands(),
 	}
 
 	app.Run(os.Args)
