8d402617ff74dfa4edbf54ec6c2fec646e903a9c
Marin Atanasov Nikolov <dnaeon@gmail.com>
2015-11-06 10:59:13 +0200

    gructl: initial commit of the "report" command

---
+++ gructl/command/report_command.go
@@ -0,0 +1,61 @@
+package command
+
+import (
+	"fmt"
+	"errors"
+
+	"code.google.com/p/go-uuid/uuid"
+
+	"github.com/codegangsta/cli"
+)
+
+func NewReportCommand() cli.Command {
+	cmd := cli.Command{
+		Name: "report",
+		Usage: "generate classifier report",
+		Action: execReportCommand,
+		},
+	}
+
+	return cmd
+}
+
+// Executes the "report" command
+func execReportCommand(c *cli.Context) {
+	if len(c.Args()) == 0 {
+		displayError(errors.New("Must provide a classifier key"), 64)
+	}
+
+	classifierKey := c.Args()[0]
+	client := newEtcdMinionClientFromFlags(c)
+
+	fmt.Printf("Generating report for classifier: %s\n", classifierKey)
+	minions, err := client.MinionWithClassifierKey(classifierKey)
+	if err != nil {
+		displayError(err, 1)
+	}
+
+	fmt.Printf("Found %d minion(s) with the given classifier", len(minions))
+
+	if len(minions) == 0 {
+		return
+	}
+
+	report := make(map[string]int)
+	for _, minion := range minions {
+		classifier, err := client.MinionClassifier(minion, classifierKey)
+		if err != nil {
+			displayError(err, 1)
+		}
+		if _, ok := report[classifier.Value]; ok {
+			report[classifier.Value] += 1
+		} else {
+			report[classifier.Value] = 1
+		}
+	}
+
+	fmt.Println("\n")
+	for k, v := range report {
+		fmt.Printf("\t%s: %d minion(s)\n", k, v)
+	}
+}
