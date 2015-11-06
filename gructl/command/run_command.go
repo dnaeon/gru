0bb4f3cffa3544d5772589aca88fe12273691f87
Marin Atanasov Nikolov <dnaeon@gmail.com>
2015-11-06 10:41:13 +0200

    Initial commit of the "run" command

---
+++ gructl/command/run_command.go
@@ -0,0 +1,83 @@
+package command
+
+import (
+	"fmt"
+	"errors"
+
+	"github.com/dnaeon/gru/task"
+
+	"code.google.com/p/go-uuid/uuid"
+	"github.com/codegangsta/cli"
+)
+
+func NewRunCommand() cli.Command {
+	cmd := cli.Command{
+		Name: "run",
+		Usage: "send task to minion(s)",
+		Action: execRunCommand,
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name: "with-classifier",
+				Value: "",
+				Usage: "target minion(s) with given classifier only",
+			},
+		},
+	}
+
+	return cmd
+}
+
+// Executes the "run" command
+func execRunCommand(c *cli.Context) {
+	if len(c.Args()) < 1 {
+		displayError(errors.New("Must provide command to run"), 64)
+	}
+
+	client := newEtcdMinionClientFromFlags(c)
+
+	// If --with-classifier flag is provided then
+	// send the task for processing only to
+	// minion(s) which contain the given classifier.
+	// Otherwise send the task to all minions
+	var err error
+	var minions []uuid.UUID
+	cFlag := c.String("with-classifier")
+	if cFlag != "" {
+		// TODO: Be able to specify a classifier patterns
+		minions, err = client.MinionWithClassifierKey(cFlag)
+	} else {
+		minions, err = client.MinionList()
+	}
+
+	if err != nil {
+		displayError(err, 1)
+	}
+
+	numMinions := len(minions)
+	if numMinions == 0 {
+		displayError(errors.New("No minion(s) found"), 1)
+	}
+
+	fmt.Printf("Found %d minion(s) for task processing\n", numMinions)
+
+	// The first argument is the command and anything else
+	// that follows is considered as task arguments
+	args := c.Args()
+	taskCommand := args[0]
+	taskArgs := args[1:]
+	t := task.New(taskCommand, taskArgs...)
+
+	failed := 0
+	for i, minion := range minions {
+		fmt.Printf("[%d/%d] Submitting task to minion %s\r", i + 1, numMinions, minion)
+		err = client.MinionSubmitTask(minion, t)
+		if err != nil {
+			failed += 1
+			fmt.Printf("\nFailed to submit task to %s: %s\n", minion, err)
+		}
+	}
+	fmt.Println()
+
+	fmt.Println("Task submitted to %d minion(s), %d of which has failed\n", numMinions, failed)
+	fmt.Println("Task results can be retrieved by using this task id: %s\n", t.TaskID)
+}
