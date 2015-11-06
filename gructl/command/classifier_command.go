0256bf4892e172d78b9c63fd577b8655272cbb33
Marin Atanasov Nikolov <dnaeon@gmail.com>
2015-11-06 10:57:54 +0200

    gructl: initial commit of the "classifier" command

---
+++ gructl/command/classifier_command.go
@@ -0,0 +1,48 @@
+package command
+
+import (
+	"fmt"
+	"errors"
+
+	"code.google.com/p/go-uuid/uuid"
+
+	"github.com/codegangsta/cli"
+)
+
+func NewClassifierCommand() cli.Command {
+	cmd := cli.Command{
+		Name: "classifier",
+		Usage: "list minion classifiers",
+		Action: execClassifierCommand,
+	},
+
+	return cmd
+}
+
+// Executes the "classifier" command
+func execClassifierCommand(c *cli.Context) {
+	if len(c.Args()) == 0 {
+		displayError(errors.New("Must provide a minion uuid"), 64)
+	}
+
+	arg := c.Args()[0]
+	minion := uuid.Parse(arg)
+	if minion == nil {
+		displayError(errors.New("Bad minion uuid given"), 64)
+	}
+
+	client := newEtcdMinionClientFromFlags(c)
+	classifierKeys, err := client.MinionClassifierKeys(minion)
+	if err != nil {
+		displayError(err, 1)
+	}
+
+	for _, key := range classifierKeys {
+		classifier, err := client.MinionClassifier(minion, key)
+		if err != nil {
+			displayError(err, 1)
+		}
+
+		fmt.Printf("%s: %s\n", classifier.Key, classifier.Value)
+	}
+}
