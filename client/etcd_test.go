841fb758e357ed99a1580a6d6cf212e963d08fea
Marin Atanasov Nikolov <dnaeon@gmail.com>
2015-12-17 14:40:52 +0200

    Start using go-vcr for recording and replaying HTTP interactions during tests

--- client/etcd_test.go
+++ client/etcd_test.go
@@ -4,11 +4,14 @@ package client
 
 import (
 	"reflect"
+	"sort"
 	"time"
 	"testing"
 
 	"github.com/dnaeon/gru/minion"
 	"github.com/dnaeon/gru/classifier"
+
+	"github.com/dnaeon/go-vcr/recorder"
 
 	"github.com/pborman/uuid"
 	"golang.org/x/net/context"
@@ -17,7 +20,7 @@ import (
 
 // Default config for etcd minions and clients
 var defaultEtcdConfig = etcdclient.Config{
-	Endpoints:               []string{"http://127.0.0.1:2379", "http:127.0.0.1:4001"},
+	Endpoints:               []string{"http://127.0.0.1:2379"},
 	Transport:               etcdclient.DefaultTransport,
 	HeaderTimeoutPerRequest: time.Second,
 }
@@ -42,33 +45,63 @@ func cleanupAfterTest(t *testing.T) {
 }
 
 func TestMinionList(t *testing.T) {
-	defer cleanupAfterTest(t)
+	// Start our recorder
+	r, err := recorder.New("fixtures/minion-list")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer r.Stop()
 
-	minions := []string{
+	// Etcd config using our transport
+	cfg := etcdclient.Config{
+		Endpoints:               []string{"http://127.0.0.1:2379"},
+		Transport:               r.Transport, // Inject our transport!
+		HeaderTimeoutPerRequest: etcdclient.DefaultRequestTimeout,
+	}
+
+	minionNames := []string{
 		"Bob", "Kevin", "Stuart",
 	}
+
 	wantMinions := []uuid.UUID{
 		uuid.Parse("f827bffd-bd9e-5441-be36-a92a51d0b79e"), // Bob
 		uuid.Parse("46ce0385-0e2b-5ede-8279-9cd98c268170"), // Kevin
 		uuid.Parse("f87cf58e-1e19-57e1-bed3-9dff5064b86a"), // Stuart
 	}
 
-	// Register our minions
-	for _, name := range minions {
-		m := minion.NewEtcdMinion(name, defaultEtcdConfig)
+	// Convert minion uuids as strings for
+	// sorting and equality testing
+	var wantMinionsAsString []string
+	for _, m := range wantMinions {
+		wantMinionsAsString = append(wantMinionsAsString, m.String())
+	}
+	sort.Strings(wantMinionsAsString)
+
+	// Register our minions in etcd
+	for _, name := range minionNames {
+		m := minion.NewEtcdMinion(name, cfg)
 		err := m.SetName(name)
 		if err != nil {
 			t.Error(err)
 		}
 	}
 
-	klient := NewEtcdMinionClient(defaultEtcdConfig)
+	// Get minions from etcd
+	klient := NewEtcdMinionClient(cfg)
 	gotMinions, err := klient.MinionList()
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if !reflect.DeepEqual(wantMinions, gotMinions) {
+	// Convert retrieved minion uuids as string for
+	// sorting and equality testing
+	var gotMinionsAsString []string
+	for _, m := range gotMinions {
+		gotMinionsAsString = append(gotMinionsAsString, m.String())
+	}
+	sort.Strings(gotMinionsAsString)
+
+	if !reflect.DeepEqual(wantMinionsAsString, gotMinionsAsString) {
 		t.Errorf("want %q minions, got %q minions", wantMinions, gotMinions)
 	}
 }
