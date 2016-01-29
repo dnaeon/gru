9250c2d1280d0593ff67616c134201b1aeeb04c4
Marin Atanasov Nikolov <dnaeon@gmail.com>
2016-01-29 13:06:31 +0200

    tests: add utils package for sharing common code between tests

---
+++ integration/utils.go
@@ -0,0 +1,40 @@
+package integration
+
+import (
+	"github.com/dnaeon/go-vcr/recorder"
+	"github.com/dnaeon/gru/client"
+
+	etcdclient "github.com/coreos/etcd/client"
+)
+
+// Minion client used during integration testing
+type testClient struct {
+	client   client.Client
+	config   etcdclient.Config
+	recorder *recorder.Recorder
+}
+
+// Creates a new etcd client with recording enabled
+func mustNewTestClient(cassette string) *testClient {
+	// Start our recorder
+	r, err := recorder.New(cassette)
+	if err != nil {
+		panic(err)
+	}
+
+	cfg := etcdclient.Config{
+		Endpoints:               []string{"http://127.0.0.1:2379"},
+		Transport:               r.Transport, // Inject our transport!
+		HeaderTimeoutPerRequest: etcdclient.DefaultRequestTimeout,
+	}
+
+	klient := client.NewEtcdMinionClient(cfg)
+
+	tc := &testClient{
+		client:   klient,
+		config:   cfg,
+		recorder: r,
+	}
+
+	return tc
+}
