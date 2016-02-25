12ce59ca0a5f6a3e7e8ff75c2456771db50b908b
Marin Atanasov Nikolov <dnaeon@gmail.com>
2016-02-25 16:11:19 +0200

    Initial commit of the "resource" package
    * This is an initial research of using resources to sketch out
      minions by using resources, e.g. package, file, service, etc. resources

---
+++ resource/resource.go
@@ -0,0 +1,25 @@
+package resource
+
+// State type represents the current and wanted state of a resource
+type State struct {
+	// Current state of the resource
+	Current string
+
+	// Wanted state of the resource
+	Want string
+}
+
+// Resource interface type
+type Resource interface {
+	// Returns the current and wanted state for a resource
+	Evaluate() (State, error)
+
+	// Creates the resource
+	Create() error
+
+	// Deletes the resource
+	Delete() error
+
+	// Updates the resource
+	Update() erorr
+}
