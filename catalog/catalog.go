0c182eb18efd5ce59cc631bc58e5816d1871bf0c
Marin Atanasov Nikolov <dnaeon@gmail.com>
2016-03-04 19:09:11 +0200

    Issue #7: Initial commit of the resource catalog

---
+++ catalog/catalog.go
@@ -0,0 +1,170 @@
+package catalog
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/dnaeon/gru/graph"
+	"github.com/dnaeon/gru/resource"
+	"github.com/hashicorp/hcl"
+	"github.com/hashicorp/hcl/hcl/ast"
+)
+
+// Catalog type contains resources loaded from a given HCL input
+type Catalog struct {
+	resources map[string]resource.Resource
+}
+
+// NewCatalog creates a new empty catalog
+func NewCatalog() *Catalog {
+	c := &Catalog{
+		resources: make(map[string]resource.Resource),
+	}
+
+	return c
+}
+
+// Add adds a new resource to the catalog
+func (c *Catalog) Add(r resource.Resource) error {
+	id := r.ID()
+
+	if c.Exists(id) {
+		return fmt.Errorf("Resource '%s' is already declared", id)
+	}
+
+	c.resources[id] = r
+
+	return nil
+}
+
+// Exists returns true if the resource id already exists in the catalog
+// Otherwise it returns false
+func (c *Catalog) Exists(id string) bool {
+	_, ok := c.resources[id]
+
+	return ok
+}
+
+// Run processes the resources from the catalog
+func (c *Catalog) Run() error {
+	// Create a DAG graph of the resources and perform
+	// topological sorting of the graph to determine the
+	// order of processing the resources
+	g := graph.NewGraph()
+
+	// A map containing the resource ids and their nodes in the graph
+	nodes := make(map[string]*graph.Node)
+
+	// Create the graph nodes for each resource from the catalog
+	for name := range c.resources {
+		node := graph.NewNode(name)
+		nodes[name] = node
+		g.AddNode(node)
+	}
+
+	// Connect the nodes in the graph
+	for name, r := range c.resources {
+		deps := r.Want()
+		for _, dep := range deps {
+			if !c.Exists(dep) {
+				e := fmt.Errorf("Resource '%s' wants '%s', which is not found in catalog", name, dep)
+				return e
+			}
+			g.AddEdge(nodes[name], nodes[dep])
+		}
+	}
+
+	// Perform topological sort of the graph
+	sorted, err := g.Sort()
+	if err != nil {
+		return err
+	}
+
+	for _, node := range sorted {
+		r := c.resources[node.Name]
+		id := r.ID()
+		log.Printf("Evaluating resource '%s'", id)
+		state, err := r.Evaluate()
+		if err != nil {
+			log.Printf("Failed to evaluate resource '%s': %s", id, err)
+			continue
+		}
+
+		if state.Want == state.Current {
+			continue
+		}
+
+		log.Printf("%s is %s, should be %s", id, state.Current, state.Want)
+		switch {
+		case state.Want == resource.Present && state.Current == resource.Absent:
+			r.Create()
+		case state.Want == resource.Absent && state.Current != resource.Absent:
+			r.Delete()
+		case state.Want == resource.Update && state.Current == resource.Present:
+			r.Update()
+		default:
+			// TODO: Validate resource states before evaluation them
+			log.Printf("Unknown state '%s' for resource '%s'", state.Want, id)
+		}
+	}
+
+	return nil
+}
+
+// Load reads a catalog from the given input and creates resources
+func Load(path string) (*Catalog, error) {
+	c := NewCatalog()
+
+	input, err := ioutil.ReadFile(path)
+	if err != nil {
+		return c, err
+	}
+
+	// Parse configuration
+	obj, err := hcl.Parse(string(input))
+	if err != nil {
+		return c, err
+	}
+
+	// Top-level node should be an object list
+	root, ok := obj.Node.(*ast.ObjectList)
+	if !ok {
+		return c, errors.New("Missing root node")
+	}
+
+	// Get the resource declarations and create the actual resources
+	resources := root.Filter("resource")
+	for _, item := range resources.Items {
+		position := item.Val.Pos().String()
+
+		// The item is expected to have exactly one key,
+		// which represents the resource type
+		if len(item.Keys) != 1 {
+			e := fmt.Errorf("Invalid resource declaration found at %s", position)
+			return c, e
+		}
+
+		// Get the resource type and create the actual resource
+		resourceType := item.Keys[0].Token.Value().(string)
+		provider, ok := resource.Get(resourceType)
+		if !ok {
+			e := fmt.Errorf("Unknown resource type '%s' found at %s", resourceType, position)
+			return c, e
+		}
+
+		// Create the actual resource
+		r, err := provider(item)
+		if err != nil {
+			return c, err
+		}
+
+		err = c.Add(r)
+		if err != nil {
+			return c, err
+		}
+	}
+
+	return c, nil
+}
