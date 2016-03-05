c0c4d4444e8ea1d285dde8d221e638c5e239bc8b
Marin Atanasov Nikolov <dnaeon@gmail.com>
2016-03-05 16:55:04 +0200

    tests: test for graphs with circular dependencies

--- graph/graph_test.go
+++ graph/graph_test.go
@@ -63,3 +63,44 @@ func TestWorkingGraph(t *testing.T) {
 		t.Errorf("Want %q, got %q graph", wantSorted, gotSorted)
 	}
 }
+
+func TestCircularGraph(t *testing.T) {
+	g := NewGraph()
+
+	// Node names
+	nodeNames := []string{
+		"A",
+		"B",
+		"C",
+		"D",
+		"E",
+	}
+
+	// Map containing the node names and the actual node instance
+	nodes := make(map[string]*Node)
+
+	// Create nodes and add them to the graph
+	for _, name := range nodeNames {
+		n := NewNode(name)
+		nodes[name] = n
+		g.AddNode(n)
+	}
+
+	// Connect the nodes in the graph
+	//
+	// A
+	// B -> C
+	// C -> D
+	// D -> E
+	// E -> D  <- Circular dependency here
+	//
+	g.AddEdge(nodes["B"], nodes["C"])
+	g.AddEdge(nodes["C"], nodes["D"])
+	g.AddEdge(nodes["D"], nodes["E"])
+	g.AddEdge(nodes["E"], nodes["D"])
+
+	_, err := g.Sort()
+	if err != ErrCircularDependency {
+		t.Errorf("want a circular dependency error, got %s", err)
+	}
+}
