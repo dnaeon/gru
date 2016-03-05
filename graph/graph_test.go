6c0d3b55455da82cafea5a94b8ed0ff9a40f0741
Marin Atanasov Nikolov <dnaeon@gmail.com>
2016-03-05 16:47:40 +0200

    tests: Add test for working topological sort of a graph

---
+++ graph/graph_test.go
@@ -0,0 +1,65 @@
+package graph
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestWorkingGraph(t *testing.T) {
+	g := NewGraph()
+
+	// Graph node names
+	nodeNames := []string{
+		"A",
+		"B",
+		"C",
+		"D",
+		"E",
+	}
+
+	// Map containing the node names and the actual node instance
+	nodes := make(map[string]*Node)
+
+	// Create nodes and add them to the graph
+	for _, name := range nodeNames {
+		n := NewNode(name)
+		nodes[name] = n
+		g.AddNode(n)
+	}
+
+	// Connect the nodes in the graph
+	//
+	// A
+	// B -> C
+	// C -> D
+	// D -> E
+	// E -> A
+	//
+	g.AddEdge(nodes["B"], nodes["C"])
+	g.AddEdge(nodes["C"], nodes["D"])
+	g.AddEdge(nodes["D"], nodes["E"])
+	g.AddEdge(nodes["E"], nodes["A"])
+
+	// Excepted result after topological sort
+	wantSorted := []string{
+		"A",
+		"E",
+		"D",
+		"C",
+		"B",
+	}
+
+	gotNodes, err := g.Sort()
+	if err != nil {
+		t.Error(err)
+	}
+
+	gotSorted := make([]string, 0)
+	for _, node := range gotNodes {
+		gotSorted = append(gotSorted, node.Name)
+	}
+
+	if !reflect.DeepEqual(wantSorted, gotSorted) {
+		t.Errorf("Want %q, got %q graph", wantSorted, gotSorted)
+	}
+}
