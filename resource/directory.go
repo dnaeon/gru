5ea5ca97d1d82efcb067c14e9fb7bac157b43f21
Marin Atanasov Nikolov <dnaeon@gmail.com>
2016-04-27 17:41:10 +0300

    Issue #24: Be able to set owner/group of target directories

--- resource/directory.go
+++ resource/directory.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"os/user"
+	"strconv"
+	"syscall"
 
 	"github.com/hashicorp/hcl"
 	"github.com/hashicorp/hcl/hcl/ast"
@@ -23,10 +26,30 @@ type DirectoryResource struct {
 
 	// Permission bits to set on the directory
 	Mode int `hcl:"mode" json:"mode"`
+
+	// Owner of the directory
+	Owner string `hcl:"owner" json:"owner"`
+
+	// Group of the directory
+	Group string `hcl:"group" json:"group"`
 }
 
 // NewDirectoryResource creates a new resource for managing directories
 func NewDirectoryResource(title string, obj *ast.ObjectItem) (Resource, error) {
+	// Defaults for owner and group
+	currentUser, err := user.Current()
+	if err != nil {
+		return nil, err
+	}
+
+	currentGroup, err := user.LookupGroupId(currentUser.Gid)
+	if err != nil {
+		return nil, err
+	}
+
+	defaultOwner := currentUser.Username
+	defaultGroup := currentGroup.Name
+
 	// Resource defaults
 	defaults := DirectoryResource{
 		BaseResource: BaseResource{
@@ -34,12 +57,14 @@ func NewDirectoryResource(title string, obj *ast.ObjectItem) (Resource, error) {
 			Type:  dirResourceType,
 			State: StatePresent,
 		},
-		Name: title,
-		Mode: 0755,
+		Name:  title,
+		Mode:  0755,
+		Owner: defaultOwner,
+		Group: defaultGroup,
 	}
 
 	var d DirectoryResource
-	err := hcl.DecodeObject(&d, obj)
+	err = hcl.DecodeObject(&d, obj)
 	if err != nil {
 		return nil, err
 	}
@@ -58,7 +83,7 @@ func (d *DirectoryResource) Evaluate() (State, error) {
 	}
 
 	// Directory does not exist
-	f, err := os.Stat(d.Name)
+	fi, err := os.Stat(d.Name)
 	if os.IsNotExist(err) {
 		resourceState.Current = StateAbsent
 
@@ -67,12 +92,34 @@ func (d *DirectoryResource) Evaluate() (State, error) {
 
 	// File exists, ensure that it is a directory
 	resourceState.Current = StatePresent
-	if !f.IsDir() {
+	if !fi.IsDir() {
 		return resourceState, fmt.Errorf("%s exists, but is not a directory", d.Name)
 	}
 
 	// Check permissions
-	if f.Mode().Perm() != os.FileMode(d.Mode) {
+	if fi.Mode().Perm() != os.FileMode(d.Mode) {
+		resourceState.Update = true
+	}
+
+	// Check ownership
+	owner, err := user.Lookup(d.Owner)
+	if err != nil {
+		return resourceState, err
+	}
+
+	group, err := user.LookupGroup(d.Group)
+	if err != nil {
+		return resourceState, err
+	}
+
+	uid, _ := strconv.Atoi(owner.Uid)
+	gid, _ := strconv.Atoi(group.Gid)
+
+	if uid != int(fi.Sys().(*syscall.Stat_t).Uid) {
+		resourceState.Update = true
+	}
+
+	if gid != int(fi.Sys().(*syscall.Stat_t).Gid) {
 		resourceState.Update = true
 	}
 
@@ -101,14 +148,39 @@ func (d *DirectoryResource) Delete(w io.Writer) error {
 
 // Update updates the permission bits of the directory
 func (d *DirectoryResource) Update(w io.Writer) error {
-	f, err := os.Stat(d.Name)
+	fi, err := os.Stat(d.Name)
 	if err != nil {
 		return err
 	}
 
-	if f.Mode().Perm() != os.FileMode(d.Mode) {
+	// Fix permissions
+	//
+	// TODO: Might want to move this to a util function, so we can
+	//       reuse it when using this resource in recursive mode
+	//
+	if fi.Mode().Perm() != os.FileMode(d.Mode) {
 		d.Printf(w, "setting permissions to %#o\n", d.Mode)
 		if err = os.Chmod(d.Name, os.FileMode(d.Mode)); err != nil {
+			return err
+		}
+	}
+
+	owner, err := user.Lookup(d.Owner)
+	if err != nil {
+		return err
+	}
+
+	group, err := user.LookupGroup(d.Group)
+	if err != nil {
+		return err
+	}
+
+	uid, _ := strconv.Atoi(owner.Uid)
+	gid, _ := strconv.Atoi(group.Gid)
+
+	if uid != int(fi.Sys().(*syscall.Stat_t).Uid) || gid != int(fi.Sys().(*syscall.Stat_t).Gid) {
+		d.Printf(w, "setting owner to %s:%s\n", d.Owner, d.Group)
+		if err = os.Chown(d.Name, uid, gid); err != nil {
 			return err
 		}
 	}
