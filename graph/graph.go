188257f3f3eb71e98dd55f533901acb246a42caa
Marin Atanasov Nikolov <dnaeon@gmail.com>
2016-05-04 17:06:21 +0300

    graph: move graph.AsDot to graph.go where it should belong

--- graph/graph.go
+++ graph/graph.go
@@ -2,6 +2,9 @@ package graph
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"strings"
 
 	mapset "github.com/deckarep/golang-set"
 )
@@ -107,4 +110,30 @@ func (g *Graph) Sort() ([]*Node, error) {
 	}
 
 	return sorted, nil
+}
+
+// AsDot generates a DOT representation for the graph
+// https://en.wikipedia.org/wiki/DOT_(graph_description_language)
+func (g *Graph) AsDot(name string, w io.Writer) {
+	var dotNodes []string
+	dotHeader := []byte(fmt.Sprintf("digraph %s {\n", name))
+	dotFooter := []byte("\n}\n")
+
+	for _, node := range g.nodes {
+		// Insert the node as the first item in the DOT object
+		var obj []string
+		obj = append(obj, fmt.Sprintf("\t%q", node.Name))
+
+		// Now add the node edges as well
+		for _, edge := range node.edges {
+			obj = append(obj, fmt.Sprintf("%q", edge.Name))
+		}
+
+		dotObj := strings.Join(obj, " -> ")
+		dotNodes = append(dotNodes, dotObj)
+	}
+
+	w.Write(dotHeader)
+	w.Write([]byte(strings.Join(dotNodes, "\n")))
+	w.Write(dotFooter)
 }
