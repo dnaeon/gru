464b7fae70e92cf81959828a8898c5a77e1a3347
Marin Atanasov Nikolov <dnaeon@gmail.com>
2016-05-04 17:07:29 +0300

    gructl: fix 'graph' command

--- gructl/command/graph.go
+++ gructl/command/graph.go
@@ -31,7 +31,7 @@ func execGraphCommand(c *cli.Context) {
 		displayError(err, 1)
 	}
 
-	err = katalog.GenerateCatalogDOT(os.Stdout)
+	err = katalog.ResourcesAsDot(os.Stdout)
 	if err != nil {
 		displayError(err, 1)
 	}
