c6a61df32109f825652000050030dc7b27120a8d
Marin Atanasov Nikolov <dnaeon@gmail.com>
2016-05-04 17:21:40 +0300

    gructl: be able to create DOT for module imports and resources as well

--- gructl/command/graph.go
+++ gructl/command/graph.go
@@ -1,10 +1,12 @@
 package command
 
 import (
+	"errors"
 	"os"
 
 	"github.com/codegangsta/cli"
 	"github.com/dnaeon/gru/catalog"
+	"github.com/dnaeon/gru/module"
 )
 
 // NewGraphCommand creates a new sub-command for
@@ -12,8 +14,18 @@ import (
 func NewGraphCommand() cli.Command {
 	cmd := cli.Command{
 		Name:   "graph",
-		Usage:  "generate a DOT file of the resources graph",
+		Usage:  "create DOT representation for modules and resources",
 		Action: execGraphCommand,
+		Flags: []cli.Flag{
+			cli.BoolFlag{
+				Name:  "modules",
+				Usage: "create DOT for module imports",
+			},
+			cli.BoolFlag{
+				Name:  "resources",
+				Usage: "create DOT for resources in catalog",
+			},
+		},
 	}
 
 	return cmd
@@ -25,14 +37,31 @@ func execGraphCommand(c *cli.Context) {
 		displayError(errNoModuleName, 64)
 	}
 
-	main := c.Args()[0]
-	katalog, err := catalog.Load(main, c.GlobalString("modulepath"))
-	if err != nil {
-		displayError(err, 1)
+	if !c.Bool("modules") && !c.Bool("resources") {
+		displayError(errors.New("Must specify either --modules or --resources flag"), 64)
 	}
 
-	err = katalog.ResourcesAsDot(os.Stdout)
-	if err != nil {
-		displayError(err, 1)
+	if c.Bool("modules") && c.Bool("resources") {
+		displayError(errors.New("Only one of --modules or --resources can be specified"), 64)
+	}
+
+	main := c.Args()[0]
+
+	// Create DOT representation for module imports
+	if c.Bool("modules") {
+		if err := module.ImportGraphAsDot(main, c.GlobalString("modulepath"), os.Stdout); err != nil {
+			displayError(err, 1)
+		}
+	} else if c.Bool("resources") {
+		// Create DOT representation for resources
+		katalog, err := catalog.Load(main, c.GlobalString("modulepath"))
+		if err != nil {
+			displayError(err, 1)
+		}
+
+		err = katalog.ResourcesAsDot(os.Stdout)
+		if err != nil {
+			displayError(err, 1)
+		}
 	}
 }
