68d432985553ac92f593821c4028a3bbb7d06bb7
Marin Atanasov Nikolov <dnaeon@gmail.com>
2016-05-05 15:46:36 +0300

    contrib: update script for building libgit2

--- contrib/misc/build-libgit2.sh
+++ contrib/misc/build-libgit2.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env sh
+#
+# Script used to build and install libgit2 on Travis CI
+#
 
 set -xe
 
-git clone https://github.com/libgit2/libgit2.git
-cd libgit2/
+LIBGIT2_VERSION="0.24.0"
+wget -O libgit2-${LIBGIT2_VERSION}.tar.gz https://github.com/libgit2/libgit2/archive/v${LIBGIT2_VERSION}.tar.gz
+tar -xzvf libgit2-${LIBGIT2_VERSION}.tar.gz
+cd libgit2-${LIBGIT2_VERSION}
 mkdir build && cd build
 cmake -DBUILD_CLAR=OFF .. && make && sudo make install
 sudo ldconfig
