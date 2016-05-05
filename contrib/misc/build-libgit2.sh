553aa286c3fdead4987d0ab185ff74a50e5cb5ee
Marin Atanasov Nikolov <dnaeon@gmail.com>
2016-05-05 15:11:54 +0300

    travis ci: do not build  Clar-based test suite for libgit2

--- contrib/misc/build-libgit2.sh
+++ contrib/misc/build-libgit2.sh
@@ -3,5 +3,5 @@
 git clone https://github.com/libgit2/libgit2.git
 cd libgit2/
 mkdir build && cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=../_install
+cmake .. -DCMAKE_INSTALL_PREFIX=../_install -DBUILD_CLAR=OFF
 cmake --build . --target install
