7c783f0e02a28c7e3fba645e147c4361f23e0cad
Marin Atanasov Nikolov <dnaeon@gmail.com>
2016-05-13 12:40:00 +0300

    resource: initial commit of the resource collections

---
+++ module/collection.go
@@ -0,0 +1,94 @@
+package module
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/dnaeon/gru/graph"
+	"github.com/dnaeon/gru/resource"
+)
+
+// ResourceMap type is a map which keys are the
+// resource ids and their values are the actual resources
+type ResourceMap map[string]resource.Resource
+
+// ResourceCollection creates a map of the unique resources
+// contained within the provided modules.
+func ResourceCollection(modules []*Module) (ResourceMap, error) {
+	moduleMap := make(map[string]string)
+	resourceMap := make(ResourceMap)
+
+	for _, m := range modules {
+		for _, r := range m.Resources {
+			id := r.ResourceID()
+			if _, ok := resourceMap[id]; ok {
+				return resourceMap, fmt.Errorf("Duplicate resource %s in %s, previous declaration was in %s", id, m.Name, moduleMap[id])
+			}
+			moduleMap[id] = m.Name
+			resourceMap[id] = r
+		}
+	}
+
+	return resourceMap, nil
+}
+
+// DependencyGraph builds a dependency graph for the resource collection
+func (rm ResourceMap) DependencyGraph() (*graph.Graph, error) {
+	g := graph.New()
+
+	// A map containing the resource ids and their nodes in the graph
+	nodes := make(map[string]*graph.Node)
+	for name := range rm {
+		node := graph.NewNode(name)
+		nodes[name] = node
+		g.AddNode(node)
+	}
+
+	// Connect the nodes in the graph
+	for name, r := range rm {
+		before := r.WantBefore()
+		after := r.WantAfter()
+
+		// Connect current resource with the ones that happen after it
+		for _, dep := range after {
+			if _, ok := rm[dep]; !ok {
+				e := fmt.Errorf("Resource %s wants %s, which does not exist", name, dep)
+				return g, e
+			}
+			g.AddEdge(nodes[name], nodes[dep])
+		}
+
+		// Connect current resource with the ones that happen before it
+		for _, dep := range before {
+			if _, ok := rm[dep]; !ok {
+				e := fmt.Errorf("Resource %s wants %s, which does not exist", name, dep)
+				return g, e
+			}
+			g.AddEdge(nodes[dep], nodes[name])
+		}
+	}
+
+	return g, nil
+}
+
+// DependencyGraphAsDot generates a DOT representation for the resources in catalog
+func (rm ResourceMap) DependencyGraphAsDot(w io.Writer) error {
+	g, err := rm.DependencyGraph()
+	if err != nil {
+		return err
+	}
+
+	g.AsDot("resources", w)
+
+	// Try a topological sort of the graph
+	// In case of circular dependencies in the graph
+	// generate a DOT file for the remaining nodes in the graph,
+	// which would give us the resources causing circular dependencies
+	if nodes, err := g.Sort(); err == graph.ErrCircularDependency {
+		circular := graph.New()
+		circular.AddNode(nodes...)
+		circular.AsDot("resources_circular", w)
+	}
+
+	return nil
+}
