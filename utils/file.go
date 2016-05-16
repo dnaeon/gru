d37459ec767fa08e3413eba43af62abe5a3a3948
Marin Atanasov Nikolov <dnaeon@gmail.com>
2016-05-16 12:39:13 +0300

    utils: implement FileUtil.Md5 method for computing the md5 hash of a file

--- utils/file.go
+++ utils/file.go
@@ -24,3 +24,13 @@ func NewFileUtil(path string) (*FileUtil, error) {
 
 	return f, nil
 }
+
+// Md5 returns the md5 hash of the file's content
+func (f *FileUtil) Md5() (string, error) {
+	buf, err := iotuil.ReadFile(f.path)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%x", md5.Sum(buf)), nil
+}
