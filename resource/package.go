eaaa44c82fbe296a41229edd7980803dd042d82a
Marin Atanasov Nikolov <dnaeon@gmail.com>
2016-05-22 17:20:27 +0300

    resource: Initial commit of the meta PackageResource type

---
+++ resource/package.go
@@ -0,0 +1,93 @@
+package resource
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/dnaeon/gru/utils"
+	"github.com/hashicorp/hcl"
+	"github.com/hashicorp/hcl/hcl/ast"
+)
+
+// Name and description of the resource
+const packageResourceType = "package"
+const packageResourceDesc = "meta resource for package management"
+
+// BasePackageResource is the base resource type for package management
+// It's purpose is to be embeded into other package resource providers.
+type BasePackageResource struct {
+	BaseResource `hcl:",squash"`
+
+	// Name of the package
+	Name string `hcl:"name"`
+
+	// Version of the package
+	Version string `hcl:"version"`
+
+	// Provider to use
+	Provider string `hcl:"provider"`
+}
+
+// NewPackageResource creates a new resource for managing packages
+func NewPackageResource(title string, obj *ast.ObjectItem, config *Config) (Resource, error) {
+	// The package providers that we know of
+	providers := map[string]Provider{
+		"pacman": NewPacmanResource,
+	}
+
+	// Releases files used by the various GNU/Linux distros
+	releases := map[string]Provider{
+		"/etc/arch-release": NewPacmanResource,
+	}
+
+	// Decode the object from HCL
+	var pr BasePackageResource
+	err := hcl.DecodeObject(&pr, obj)
+	if err != nil {
+		return nil, err
+	}
+
+	// If we have a provider for this resource, use it
+	if pr.Provider != "" {
+		provider, ok := providers[pr.Provider]
+		if !ok {
+			return nil, fmt.Errorf("Unknown package provider '%s'", pr.Provider)
+		}
+
+		r, err := provider(title, obj, config)
+		if err != nil {
+			return nil, err
+		}
+
+		// Replace the resource type with our meta type
+		r.SetType(packageResourceType)
+		return r, nil
+	}
+
+	// Do our best to determine the proper provider for this resource
+	for release, provider := range releases {
+		dst := utils.NewFileUtil(release)
+		if dst.Exists() {
+			r, err := provider(title, obj, config)
+			if err != nil {
+				return nil, err
+			}
+
+			// Replace the resource type with our meta type
+			r.SetType(packageResourceType)
+			return r, nil
+		}
+	}
+
+	return nil, errors.New("Unable to find suitable package provider")
+}
+
+func init() {
+	item := RegistryItem{
+		Name:        packageResourceType,
+		Description: packageResourceDesc,
+		Provider:    NewPackageResource,
+	}
+
+	Register(item)
+}
