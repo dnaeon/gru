5e72446d938ee6f688e1a95441fa17cede8264d9
Marin Atanasov Nikolov <dnaeon@gmail.com>
2016-05-26 17:38:16 +0300

    gructl: 'graph' command generates a dot for both modules and resources

--- gructl/command/graph.go
+++ gructl/command/graph.go
@@ -1,12 +1,13 @@
 package command
 
 import (
-	"errors"
+	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/codegangsta/cli"
-	"github.com/dnaeon/gru/catalog"
+	"github.com/dnaeon/gru/graph"
 	"github.com/dnaeon/gru/module"
 	"github.com/dnaeon/gru/resource"
 )
@@ -19,14 +20,6 @@ func NewGraphCommand() cli.Command {
 		Usage:  "create DOT representation for modules and resources",
 		Action: execGraphCommand,
 		Flags: []cli.Flag{
-			cli.BoolFlag{
-				Name:  "modules",
-				Usage: "create DOT for module imports",
-			},
-			cli.BoolFlag{
-				Name:  "resources",
-				Usage: "create DOT for resources in catalog",
-			},
 			cli.StringFlag{
 				Name:   "siterepo",
 				Value:  "",
@@ -45,48 +38,93 @@ func execGraphCommand(c *cli.Context) {
 		displayError(errNoModuleName, 64)
 	}
 
-	if !c.Bool("modules") && !c.Bool("resources") {
-		displayError(errors.New("Must specify either --modules or --resources flag"), 64)
-	}
-
-	if c.Bool("modules") && c.Bool("resources") {
-		displayError(errors.New("Only one of --modules or --resources can be specified"), 64)
-	}
-
 	main := c.Args()[0]
-	modulePath := filepath.Join(c.String("siterepo"), "modules")
+	config := &module.Config{
+		Path:           filepath.Join(c.String("siterepo"), "modules"),
+		ResourceConfig: &resource.Config{},
+	}
 
-	// Create DOT representation for module imports
-	if c.Bool("modules") {
-		if err := module.ImportGraphAsDot(main, modulePath, os.Stdout); err != nil {
-			displayError(err, 1)
+	// Discover and load all modules
+	discovered, err := module.DiscoverAndLoad(config)
+	if err != nil {
+		displayError(err, 1)
+	}
+
+	// Generate DOT representation of the module imports
+	modulesGraph, err := module.ImportGraph(main, config.Path)
+	if err != nil {
+		displayError(err, 1)
+	}
+	modulesGraph.AsDot(fmt.Sprintf("%s_imports", main), os.Stdout)
+
+	// Attempt a topological sort of the module imports graph.
+	// In case of circular dependencies in the graph
+	// generate a DOT for the remaining nodes, which would
+	// give us the nodes causing the circular dependencies
+	modulesSorted, err := modulesGraph.Sort()
+	if err == graph.ErrCircularDependency {
+		circular := graph.New()
+		circular.AddNode(modulesSorted...)
+		circular.AsDot(fmt.Sprintf("%s_imports_circular", main), os.Stdout)
+		displayError(graph.ErrCircularDependency, 1)
+	}
+
+	// Get the sorted modules and create a DOT for their resources
+	var modules []*module.Module
+	for _, node := range modulesSorted {
+		modules = append(modules, discovered[node.Name])
+	}
+
+	collection, err := module.ResourceCollection(modules)
+	if err != nil {
+		displayError(err, 1)
+	}
+
+	collectionGraph, err := collection.DependencyGraph()
+	if err != nil {
+		displayError(err, 1)
+	}
+
+	// Create resource nodes in their respective module
+	fmt.Fprintf(os.Stdout, "digraph resources {\n")
+	fmt.Fprintf(os.Stdout, "\tlabel = \"resources\";\n")
+	fmt.Fprintf(os.Stdout, "\tnodesep=1.0;\n")
+	fmt.Fprintf(os.Stdout, "\tnode [shape=box];\n")
+	fmt.Fprintf(os.Stdout, "\tedge [style=filled];\n")
+	for _, m := range modules {
+		fmt.Fprintf(os.Stdout, fmt.Sprintf("\tsubgraph cluster_%s {\n", m.Name))
+		fmt.Fprintf(os.Stdout, fmt.Sprintf("\t\tlabel = %q;\n", m.Name))
+		fmt.Fprintf(os.Stdout, "\t\tcolor = black;\n")
+		for _, r := range m.Resources {
+			fmt.Fprintf(os.Stdout, fmt.Sprintf("\t\t%q;\n", r.ResourceID()))
 		}
-	} else if c.Bool("resources") {
-		// Create DOT representation for resources from catalog
-		config := &catalog.Config{
-			Main:   main,
-			DryRun: true,
-			ModuleConfig: &module.Config{
-				Path: modulePath,
-				ResourceConfig: &resource.Config{
-					SiteRepo: c.String("siterepo"),
-					Writer:   os.Stdout,
-				},
-			},
+		fmt.Fprintf(os.Stdout, "\t}\n")
+	}
+
+	// Finally, connect the nodes
+	for _, node := range collectionGraph.Nodes {
+		if len(node.Edges) == 0 {
+			continue
 		}
 
-		katalog, err := catalog.Load(config)
-		if err != nil {
-			displayError(err, 1)
+		var edges []string
+		for _, edge := range node.Edges {
+			edges = append(edges, fmt.Sprintf("%q", edge.Name))
 		}
 
-		collection, err := module.ResourceCollection(katalog.Modules)
-		if err != nil {
-			displayError(err, 1)
-		}
+		fmt.Fprintf(os.Stdout, fmt.Sprintf("\t%q -> {%s};\n", node.Name, strings.Join(edges, " ")))
+	}
+	fmt.Fprintf(os.Stdout, "}\n")
 
-		if err := collection.DependencyGraphAsDot(os.Stdout); err != nil {
-			displayError(err, 1)
-		}
+	// Attempt a topological sort of the resource dependencies.
+	// In case of circular dependencies in the graph
+	// generate a DOT for the remaining nodes, which would
+	// give us the resources causing the circular dependencies
+	collectionSorted, err := collectionGraph.Sort()
+	if err == graph.ErrCircularDependency {
+		circular := graph.New()
+		circular.AddNode(collectionSorted...)
+		circular.AsDot("resources_circular", os.Stdout)
+		displayError(graph.ErrCircularDependency, 1)
 	}
 }
