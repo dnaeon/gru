6aecf3f9d4f5914a3f66b8a5dbd8029f3c93585b
Marin Atanasov Nikolov <dnaeon@gmail.com>
2016-06-21 13:00:18 +0300

    resource: cleanup resource.go after moving stuff to provider.go

--- resource/resource.go
+++ resource/resource.go
@@ -3,39 +3,7 @@ package resource
 import (
 	"fmt"
 	"io"
-
-	"github.com/hashicorp/hcl/hcl/ast"
 )
-
-// Registry contains all known resources
-var Registry = make(map[string]RegistryItem)
-
-// Provider type creates new resources from an HCL AST object item
-type Provider func(title string, item *ast.ObjectItem, config *Config) (Resource, error)
-
-// RegistryItem type represents an item from the registry
-type RegistryItem struct {
-	// Name of the resource type
-	Name string
-
-	// Short desription of the resource
-	Description string
-
-	// Resource provider
-	Provider Provider
-}
-
-// Register adds a resource type to the registry
-func Register(item RegistryItem) error {
-	_, ok := Registry[item.Name]
-	if ok {
-		return fmt.Errorf("Resource type '%s' is already registered", item.Name)
-	}
-
-	Registry[item.Name] = item
-
-	return nil
-}
 
 // Resource is the interface type for resources
 type Resource interface {
