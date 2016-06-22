20d3f78d94162b09b0c44f20e63319e9494e42dc
Marin Atanasov Nikolov <dnaeon@gmail.com>
2016-06-22 15:41:44 +0300

    resource: wrap provider functions before registering them in Lua
    By wrapping the resource providers we can properly check for any
    errors that could be returned by the providers, and since we don't
    want to pass the error as lua.LValue to Lua this is the place
    where we should be handling any errors from providers.

--- resource/lua.go
+++ resource/lua.go
@@ -5,12 +5,25 @@ import (
 	"github.com/yuin/gopher-lua"
 )
 
-// RegisterLuaBuiltins registers resource providers in Lua
-func RegisterLuaBuiltins(L *lua.LState) error {
-	// Register resource providers
+// LuaRegisterBuiltin registers resource providers in Lua
+func LuaRegisterBuiltin(L *lua.LState) error {
 	for typ, provider := range providerRegistry {
+		// Wrap resource providers, so that we can properly handle any
+		// errors returned by providers during resource instantiation.
+		// Since we don't want to return the error to Lua, this is the
+		// place where we handle any errors returned by providers.
+		wrapper := func(L *lua.LState) int {
+			r, err := provider(L.CheckString(1))
+			if err != nil {
+				L.RaiseError(err.Error())
+			}
+
+			L.Push(luar.New(L, r))
+			return 1
+		}
+
 		tbl := L.NewTable()
-		tbl.RawSetH(lua.LString("new"), luar.New(L, provider))
+		tbl.RawSetH(lua.LString("new"), L.NewFunction(wrapper))
 		L.SetGlobal(typ, tbl)
 	}
 }
