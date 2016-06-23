1a6250fedbd777459eaec4b118db255c6a737e1e
Marin Atanasov Nikolov <dnaeon@gmail.com>
2016-06-23 15:27:53 +0300

    resource: collections are now part of the resource package

---
+++ resource/collection.go
@@ -0,0 +1,63 @@
+package resource
+
+import (
+	"fmt"
+
+	"github.com/dnaeon/gru/graph"
+)
+
+// Collection type is a map which keys are the
+// resource ids and their values are the actual resources
+type Collection map[string]Resource
+
+// CreateCollection creates a map from
+func CreateCollection(resources []Resource) (Collection, error) {
+	c := make(Collection)
+
+	for _, r := range resources {
+		id := r.ID()
+		if _, ok := c[id]; ok {
+			return c, fmt.Errorf("Duplicate resource declaration for %s", id)
+		}
+		c[id] = r
+	}
+
+	return c, nil
+}
+
+// DependencyGraph builds a dependency graph for the collection
+func (c Collection) DependencyGraph() (*graph.Graph, error) {
+	g := graph.New()
+
+	// A map containing the resource ids and their nodes in the graph
+	nodes := make(map[string]*graph.Node)
+	for id := range c {
+		node := graph.NewNode(id)
+		nodes[id] = node
+		g.AddNode(node)
+	}
+
+	// Connect the nodes in the graph
+	for id, r := range c {
+		before := r.WantBefore()
+		after := r.WantAfter()
+
+		// Connect current resource with the ones that happen after it
+		for _, dep := range after {
+			if _, ok := c[dep]; !ok {
+				return g, fmt.Errorf("%s wants %s, which does not exist", id, dep)
+			}
+			g.AddEdge(nodes[id], nodes[dep])
+		}
+
+		// Connect current resource with the ones that happen before it
+		for _, dep := range before {
+			if _, ok := c[dep]; !ok {
+				return g, fmt.Errorf("%s wants %s, which does not exist", id, dep)
+			}
+			g.AddEdge(nodes[dep], nodes[id])
+		}
+	}
+
+	return g, nil
+}
