7389bea7c3c144aefed99e25d551eff30aeda29d
Marin Atanasov Nikolov <dnaeon@gmail.com>
2016-06-24 17:34:32 +0300

    resource: Fix Lua resource wrapper function

--- resource/lua.go
+++ resource/lua.go
@@ -12,18 +12,21 @@ func LuaRegisterBuiltin(L *lua.LState) {
 		// errors returned by providers during resource instantiation.
 		// Since we don't want to return the error to Lua, this is the
 		// place where we handle any errors returned by providers.
-		wrapper := func(L *lua.LState) int {
-			r, err := provider(L.CheckString(1))
-			if err != nil {
-				L.RaiseError(err.Error())
-			}
+		wrapper := func(p Provider) lua.LGFunction {
+			return func(L *lua.LState) int {
+				// Create the resource by calling it's provider
+				r, err := p(L.CheckString(1))
+				if err != nil {
+					L.RaiseError(err.Error())
+				}
 
-			L.Push(luar.New(L, r))
-			return 1
+				L.Push(luar.New(L, r))
+				return 1 // Number of arguments returned to Lua
+			}
 		}
 
 		tbl := L.NewTable()
-		tbl.RawSetH(lua.LString("new"), L.NewFunction(wrapper))
+		tbl.RawSetH(lua.LString("new"), L.NewFunction(wrapper(provider)))
 		L.SetGlobal(typ, tbl)
 	}
 }
