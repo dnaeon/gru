1300981a276f717b4a0ce6784e9ab5e87f214e0f
Marin Atanasov Nikolov <dnaeon@gmail.com>
2016-06-24 11:37:59 +0300

    gructl: update 'graph' command after recent refactoring

--- gructl/command/graph.go
+++ gructl/command/graph.go
@@ -1,15 +1,14 @@
 package command
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/codegangsta/cli"
 	"github.com/dnaeon/gru/graph"
-	"github.com/dnaeon/gru/module"
 	"github.com/dnaeon/gru/resource"
+	"github.com/layeh/gopher-luar"
+	"github.com/yuin/gopher-lua"
 )
 
 // NewGraphCommand creates a new sub-command for
@@ -17,7 +16,7 @@ import (
 func NewGraphCommand() cli.Command {
 	cmd := cli.Command{
 		Name:   "graph",
-		Usage:  "create DOT representation for modules and resources",
+		Usage:  "create DOT representation of resources",
 		Action: execGraphCommand,
 		Flags: []cli.Flag{
 			cli.StringFlag{
@@ -38,44 +37,21 @@ func execGraphCommand(c *cli.Context) {
 		displayError(errNoModuleName, 64)
 	}
 
-	main := c.Args()[0]
-	config := &module.Config{
-		Path:           filepath.Join(c.String("siterepo"), "modules"),
-		ResourceConfig: &resource.Config{},
-	}
+	module := filepath.Join(c.String("siterepo"), c.Args()[0])
 
-	// Discover and load all modules
-	discovered, err := module.DiscoverAndLoad(config)
-	if err != nil {
+	// A fake catalog used to load resources from Lua
+	var sorted []resource.Resource
+
+	L := lua.NewState()
+	defer L.Close()
+
+	resource.LuaRegisterBuiltin(L)
+	L.SetGlobal("catalog", luar.New(L, unsorted))
+	if err := L.DoFile(module); err != nil {
 		displayError(err, 1)
 	}
 
-	// Generate DOT representation of the module imports
-	modulesGraph, err := module.ImportGraph(main, config.Path)
-	if err != nil {
-		displayError(err, 1)
-	}
-	modulesGraph.AsDot(fmt.Sprintf("%s_imports", main), os.Stdout)
-
-	// Attempt a topological sort of the module imports graph.
-	// In case of circular dependencies in the graph
-	// generate a DOT for the remaining nodes, which would
-	// give us the nodes causing the circular dependencies
-	modulesSorted, err := modulesGraph.Sort()
-	if err == graph.ErrCircularDependency {
-		circular := graph.New()
-		circular.AddNode(modulesSorted...)
-		circular.AsDot(fmt.Sprintf("%s_imports_circular", main), os.Stdout)
-		displayError(graph.ErrCircularDependency, 1)
-	}
-
-	// Get the sorted modules and create a DOT for their resources
-	var modules []*module.Module
-	for _, node := range modulesSorted {
-		modules = append(modules, discovered[node.Name])
-	}
-
-	collection, err := module.ResourceCollection(modules)
+	collection, err := resource.CreateCollection(unsorted)
 	if err != nil {
 		displayError(err, 1)
 	}
@@ -84,42 +60,8 @@ func execGraphCommand(c *cli.Context) {
 	if err != nil {
 		displayError(err, 1)
 	}
+	collectionGraph.AsDot("resources", os.Stdout)
 
-	// Create resource nodes in their respective module
-	fmt.Fprintf(os.Stdout, "digraph resources {\n")
-	fmt.Fprintf(os.Stdout, "\tlabel = \"resources\";\n")
-	fmt.Fprintf(os.Stdout, "\tnodesep=1.0;\n")
-	fmt.Fprintf(os.Stdout, "\tnode [shape=box];\n")
-	fmt.Fprintf(os.Stdout, "\tedge [style=filled];\n")
-	for _, m := range modules {
-		fmt.Fprintf(os.Stdout, fmt.Sprintf("\tsubgraph cluster_%s {\n", m.Name))
-		fmt.Fprintf(os.Stdout, fmt.Sprintf("\t\tlabel = %q;\n", m.Name))
-		fmt.Fprintf(os.Stdout, "\t\tcolor = black;\n")
-		for _, r := range m.Resources {
-			fmt.Fprintf(os.Stdout, fmt.Sprintf("\t\t%q;\n", r.ResourceID()))
-		}
-		fmt.Fprintf(os.Stdout, "\t}\n")
-	}
-
-	// Finally, connect the nodes
-	for _, node := range collectionGraph.Nodes {
-		if len(node.Edges) == 0 {
-			continue
-		}
-
-		var edges []string
-		for _, edge := range node.Edges {
-			edges = append(edges, fmt.Sprintf("%q", edge.Name))
-		}
-
-		fmt.Fprintf(os.Stdout, fmt.Sprintf("\t%q -> {%s};\n", node.Name, strings.Join(edges, " ")))
-	}
-	fmt.Fprintf(os.Stdout, "}\n")
-
-	// Attempt a topological sort of the resource dependencies.
-	// In case of circular dependencies in the graph
-	// generate a DOT for the remaining nodes, which would
-	// give us the resources causing the circular dependencies
 	collectionSorted, err := collectionGraph.Sort()
 	if err == graph.ErrCircularDependency {
 		circular := graph.New()
