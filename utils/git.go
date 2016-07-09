979c98ce5a17738910a9b4d57197dfac901f6883
Marin Atanasov Nikolov <dnaeon@gmail.com>
2016-07-09 13:45:20 +0300

    utils: implement Fetch and Pull methods for GitRepo type

--- utils/git.go
+++ utils/git.go
@@ -1,5 +1,7 @@
 package utils
 
+import "os/exec"
+
 // GitRepo type manages a VCS repository with Git
 type GitRepo struct {
 	// Local path to the repository
@@ -7,12 +9,39 @@ type GitRepo struct {
 
 	// Upstream URL of the Git repository
 	Upstream string
+
+	// Path to the Git tool
+	git string
 }
 
 // NewGitRepo creates a new Git repository
-func NewGitRepo(path, upstream string) *GitRepo {
-	return &GitRepo{
+func NewGitRepo(path, upstream string) (*GitRepo, error) {
+	path, err := exec.LookPath("git")
+	if err != nil {
+		return nil, err
+	}
+
+	repo := &GitRepo{
 		Path:     path,
 		Upstream: upstream,
+		cmd:      path,
 	}
+
+	return repo, nil
+}
+
+// Fetch fetches from the given remote
+func (gr *GitRepo) Fetch(remote string) ([]byte, error) {
+	return exec.Command(gr.git, "fetch", remote).CombinedOutput()
+}
+
+// Pull pulls from the given remote and merges changes into the
+// local branch
+func (gr *GitRepo) Pull(remote, branch string) ([]byte, error) {
+	out, err := exec.Command(gr.git, "checkout", branch).CombinedOut()
+	if err != nil {
+		return out, err
+	}
+
+	return exec.Command(gr.git, "pull", remote).CombinedOutput()
 }
