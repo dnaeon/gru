e23bedc1f6109ba27d19a769fe1667724e195b60
Marin Atanasov Nikolov <dnaeon@gmail.com>
2016-07-22 14:01:10 +0300

    catalog: initial support for concurrent resource processing
    At the moment only resources with no dependencies and
    which support multiple instances of the same resource type
    to be concurrent will be processed concurrently.
    
    TODO: Need to implement concurrent processing for resources,
    which have satisfied dependencies.

--- catalog/catalog.go
+++ catalog/catalog.go
@@ -3,6 +3,7 @@ package catalog
 import (
 	"fmt"
 	"log"
+	"sync"
 
 	"github.com/dnaeon/gru/resource"
 	"github.com/dnaeon/gru/utils"
@@ -18,11 +19,8 @@ type Catalog struct {
 	// Sorted contains the list of resources after a topological sort
 	sorted []resource.Resource `luar:"-"`
 
-	// Result contains the results of resource processing and any
-	// errors that might have occurred during processing.
-	// Keys of the map are the resource ids and their
-	// values are the errors returned from resources.
-	result map[string]error `luar:"-"`
+	// Status contains status information about resources
+	status *status `luar:"-"`
 
 	// Configuration settings
 	config *Config `luar:"-"`
@@ -45,14 +43,46 @@ type Config struct {
 
 	// The Lua state
 	L *lua.LState
+
+	// Number of goroutines to use for concurrent processing
+	Concurrency int
+}
+
+// status type contains status information about processed resources
+type status struct {
+	sync.RWMutex
+
+	// Items contain the result of resource processing and any
+	// errors that might have occurred during processing.
+	// Keys of the map are the resource ids and their
+	// values are the errors returned by resources.
+	items map[string]error
+}
+
+// set sets the status for a resource
+func (s *status) set(id string, err error) {
+	s.Lock()
+	defer s.Unlock()
+	s.items[id] = err
+}
+
+// get retrieves the status of a resource
+func (s *status) get(id string) (error, bool) {
+	s.Lock()
+	defer s.Unlock()
+	err, ok := s.items[id]
+
+	return err, ok
 }
 
 // New creates a new empty catalog with the provided configuration
 func New(config *Config) *Catalog {
 	c := &Catalog{
-		config:   config,
-		sorted:   make([]resource.Resource, 0),
-		result:   make(map[string]error),
+		config: config,
+		sorted: make([]resource.Resource, 0),
+		status: &status{
+			items: make(map[string]error),
+		},
 		Unsorted: make([]resource.Resource, 0),
 	}
 
@@ -117,45 +147,70 @@ func (c *Catalog) Load() error {
 		c.sorted = append(c.sorted, collection[node.Name])
 	}
 
+	c.config.Logger.Printf("Loaded %d resources\n", len(c.sorted))
+
 	return nil
 }
 
 // Run processes the resources from catalog
 func (c *Catalog) Run() error {
-	c.config.Logger.Printf("Loaded %d resources\n", len(c.sorted))
-	for _, r := range c.sorted {
+	// process executes a single resource
+	process := func(r resource.Resource) {
 		id := r.ID()
-
-		// Skip resource
-		if err := c.shouldBeSkipped(r); err != nil {
-			c.result[id] = err
-			c.config.Logger.Printf("%s skipping resource: %s\n", id, err)
-			continue
-		}
-
-		// Process resource and save result
-		if c.result[id] = c.processResource(r); c.result[id] != nil {
-			c.config.Logger.Printf("%s %s\n", id, c.result[id])
+		err := c.execute(r)
+		c.status.set(id, err)
+		if err != nil {
+			c.config.Logger.Printf("%s %s\n", id, err)
 		}
 	}
+
+	// Start goroutines for concurrent processing
+	var wg sync.WaitGroup
+	ch := make(chan resource.Resource, 1024)
+	c.config.Logger.Printf("Starting %d goroutines for concurrent processing\n", c.config.Concurrency)
+	for i := 0; i < c.config.Concurrency; i++ {
+		wg.Add(1)
+		worker := func() {
+			defer wg.Done()
+			for r := range ch {
+				c.config.Logger.Printf("%s is concurrent", r.ID())
+				process(r)
+			}
+		}
+		go worker()
+	}
+
+	// Process the resources
+	for _, r := range c.sorted {
+		switch {
+		// Resource supports concurrency and has no dependencies
+		case r.IsConcurrent() && len(r.Dependencies()) == 0:
+			ch <- r
+			continue
+
+		// TODO: Handle concurrently resources with satisfied dependencies
+
+		// Resource is not concurrent
+		default:
+			process(r)
+		}
+	}
+
+	close(ch)
+	wg.Wait()
 
 	return nil
 }
 
-// shouldBeSkipped checks if a resource has failed
-// dependencies and should be skipped from further processing.
-func (c *Catalog) shouldBeSkipped(r resource.Resource) error {
+// execute processes a single resource
+func (c *Catalog) execute(r resource.Resource) error {
+	// Check if the resource has failed dependencies
 	for _, dep := range r.Dependencies() {
-		if c.result[dep] != nil {
+		if err, _ := c.status.get(dep); err != nil {
 			return fmt.Errorf("failed dependency for %s", dep)
 		}
 	}
 
-	return nil
-}
-
-// processResource processes a single resource
-func (c *Catalog) processResource(r resource.Resource) error {
 	if err := r.Validate(); err != nil {
 		return err
 	}
