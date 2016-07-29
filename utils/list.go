aaa366d82a268aede2004bb5eb5f354a2e85f617
Marin Atanasov Nikolov <dnaeon@gmail.com>
2016-07-29 15:10:43 +0300

    utils: move list-like types to list.go

---
+++ utils/list.go
@@ -0,0 +1,44 @@
+package utils
+
+// List type represents a slice of strings
+type List []string
+
+// NewList creates a new list with the given items
+func NewList(s ...string) List {
+	l := make(List, len(s))
+	for _, v := range s {
+		l = append(l, v)
+	}
+
+	return l
+}
+
+// Contains returns a boolean indicating whether the list
+// contains the given string.
+func (l List) Contains(x string) bool {
+	for _, v := range l {
+		if v == x {
+			return true
+		}
+	}
+
+	return false
+}
+
+// String type represents a string
+type String struct {
+	str string
+}
+
+// NewString creates a new string
+func NewString(s string) String {
+	return String{
+		str: s,
+	}
+}
+
+// IsInList returns a boolean indicating whether the string is
+// contained within a given list
+func (s String) IsInList(l List) bool {
+	return l.Contains(s.str)
+}
