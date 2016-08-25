3ea8784e003bf635f08c502fad90d6cab6dea98e
Marin Atanasov Nikolov <dnaeon@gmail.com>
2016-08-25 11:05:26 +0300

    resource: doc comment update

--- resource/provider.go
+++ resource/provider.go
@@ -6,7 +6,7 @@ var providerRegistry = make([]RegistryItem, 0)
 // Provider type is the type which creates new resources
 type Provider func(name string) (Resource, error)
 
-// ProviderItem type represents a single item from the
+// RegistryItem type represents a single item from the
 // provider registry
 type RegistryItem struct {
 	// Type name of the provider
