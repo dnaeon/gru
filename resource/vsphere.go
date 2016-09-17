8f8bcd08abf0d8691452ea79afa91abb868382c2
Marin Atanasov Nikolov <dnaeon@gmail.com>
2016-09-17 15:08:35 +0300

    resource: commit vSphere related errors

--- resource/vsphere.go
+++ resource/vsphere.go
@@ -2,11 +2,27 @@ package resource
 
 import (
 	"context"
+	"errors"
 	"net/url"
 
 	"github.com/vmware/govmomi"
 	"github.com/vmware/govmomi/find"
 )
+
+// ErrNoUsername error is returned when no username is provided for
+// establishing a connection to the remote VMware vSphere API endpoint.
+var ErrNoUsername = errors.New("No username provided")
+
+// ErrNoPassword error is returned when no password is provided for
+// establishing a connection to the remote VMware vSphere API endpoint.
+var ErrNoPassword = errors.New("No password provided")
+
+// ErrNoEndpoint error is returned when no VMware vSphere API endpoint is
+// provided.
+var ErrNoEndpoint = errors.New("No endpoint provided")
+
+// ErrNotVC error is returned when the remote endpoint is not a vCenter system.
+var ErrNotVC = errors.New("Not a VMware vCenter endpoint")
 
 // BaseVSphere type is the base type for all vSphere related resources.
 type BaseVSphere struct {
