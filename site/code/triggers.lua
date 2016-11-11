b1192011d0b61f83e46c5210aadf2a1a8a7d8980
Marin Atanasov Nikolov <dnaeon@gmail.com>
2016-11-11 14:59:22 +0200

    site: add example Lua code for using resource triggers

---
+++ site/code/triggers.lua
@@ -0,0 +1,30 @@
+-- Manage the SNMP package
+pkg = resource.package.new("net-snmp")
+pkg.state = "present"
+
+-- Manage the config file for SNMP daemon
+config = resource.file.new("/etc/snmp/snmpd.conf")
+config.state = "present"
+config.content = "rocommunity public"
+config.require = { pkg:ID() }
+
+-- Manage the SNMP service
+svc = resource.service.new("snmpd")
+svc.state = "running"
+svc.enable = true
+svc.require = { pkg:ID(), config:ID() }
+
+-- Subscribe for changes in the config file resource.
+-- Reload the SNMP daemon service if the config file has changed.
+svc.subscribe[pkg:ID()] = function()
+   os.execute("systemctl reload snmpd")
+end
+
+-- Subscribe for changes in the package resource.
+-- Restart the SNMP daemon service if the package has changed.
+svc.subscribe[config:ID()] = function()
+   os.execute("systemctl restart snmpd")
+end
+
+-- Add resources to the catalog
+catalog:add(pkg, config, svc)
