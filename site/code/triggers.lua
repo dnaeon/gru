db66c8c55e10c88ed37ba17fce347fdb4662ba43
Marin Atanasov Nikolov <dnaeon@gmail.com>
2016-11-14 13:09:12 +0200

    site: typo fixes in trigger example code

--- site/code/triggers.lua
+++ site/code/triggers.lua
@@ -1,3 +1,7 @@
+--
+-- Example code for using triggers in resources
+--
+
 -- Manage the SNMP package
 pkg = resource.package.new("net-snmp")
 pkg.state = "present"
@@ -16,13 +20,13 @@ svc.require = { pkg:ID(), config:ID() }
 
 -- Subscribe for changes in the config file resource.
 -- Reload the SNMP daemon service if the config file has changed.
-svc.subscribe[pkg:ID()] = function()
+svc.subscribe[config:ID()] = function()
    os.execute("systemctl reload snmpd")
 end
 
 -- Subscribe for changes in the package resource.
 -- Restart the SNMP daemon service if the package has changed.
-svc.subscribe[config:ID()] = function()
+svc.subscribe[pkg:ID()] = function()
    os.execute("systemctl restart snmpd")
 end
 
