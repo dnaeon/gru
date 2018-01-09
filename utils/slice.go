f27c5b12cb30bd02491c4b689f1df7273c66ae49
Marin Atanasov Nikolov <mnikolov@vmware.com>
2018-01-09 15:49:25 +0200

    Fix deadlock

--- utils/slice.go
+++ utils/slice.go
@@ -65,7 +65,7 @@ func (cs *ConcurrentSlice) Iter() <-chan ConcurrentSliceItem {
 
 	f := func() {
 		cs.Lock()
-		defer cs.Lock()
+		defer cs.Unlock()
 		for index, value := range cs.items {
 			c <- ConcurrentSliceItem{index, value}
 		}
